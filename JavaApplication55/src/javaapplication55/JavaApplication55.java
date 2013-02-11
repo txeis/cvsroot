53e370e566793ce4169f36e054dbc9d10df223dc
Alumno <Alumno@INF703>
2013-02-11 13:03:59 +0100

    Primer proyecto

---
+++ JavaApplication55/src/javaapplication55/JavaApplication55.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication55;
+
+/**
+ * @author Alumno
+ */
+public class JavaApplication55 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+}
